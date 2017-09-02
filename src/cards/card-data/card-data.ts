9b3deff04292eee730164f3548905e035f54bf94
Alvaro Saburido <alvaro.saburido@gmail.com>
2017-09-02 04:28:26 +0200

    feature/CAN-34 Card classes

--- src/cards/card-data/card-data.ts
+++ src/cards/card-data/card-data.ts
@@ -1,11 +1,11 @@
 
 import { CardBase } from './../card-base';
-export class Card extends CardBase {
-
+export class CardData extends CardBase {
+    public img?: string;
     constructor(
         options: {} = {}
     ) {
         super(options);
-
+        this.img = options['img'] || null;
     }
 }
