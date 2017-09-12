271a83cd47686d942daa107294186342e8e81044
Alvaro Saburido <alvaro.saburido@gmail.com>
2017-09-12 19:44:51 +0200

    feature/CAN-31 Fixed hover css class

--- src/buttons/button.directive.ts
+++ src/buttons/button.directive.ts
@@ -5,26 +5,29 @@ import { Directive, ElementRef, Renderer2, Input, HostListener, OnInit} from '@a
 })
 export class AsButtonDirective implements OnInit {
     @Input() color = 'default';
+    @Input() size;
     @Input() outline;
     @Input() rounded;
+    @Input() block;
     @Input('asButton') asButton ;
 
   constructor(public renderer: Renderer2, public hostElement: ElementRef) {
   }
   public ngOnInit() {
     this.renderer.addClass(this.hostElement.nativeElement, `btn`);
-    if (this.outline !== undefined) {
-        this.renderer.addClass(this.hostElement.nativeElement, `btn-outline-${this.color}`);
-    }else {
-        this.renderer.addClass(this.hostElement.nativeElement, `btn-${this.color}`);
-    }
     if (this.rounded !== undefined) {
         this.renderer.addClass(this.hostElement.nativeElement, `btn-rounded`);
-    }else {
+    }
+    if (this.block !== undefined) {
+        this.renderer.addClass(this.hostElement.nativeElement, `btn-block`);
+    }
+    if (this.size !== undefined) {
+        this.renderer.addClass(this.hostElement.nativeElement, `btn-${this.size}`);
+    }
+    if (this.outline !== undefined) {
+        this.renderer.addClass(this.hostElement.nativeElement, `btn-outline-${this.color}`);
+    } else {
         this.renderer.addClass(this.hostElement.nativeElement, `btn-${this.color}`);
     }
-  }
-  @HostListener('mouseenter') onMouseEnter() {
-    console.log(this.renderer);
   }
 }
