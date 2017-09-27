7fe89ca748fb767c5d9dbab2f62dae606a07c975
Alvaro Saburido <alvaro.saburido@gmail.com>
2017-09-27 23:42:53 +0200

    feature/CAN-51 Lot of loaders and spinners

---
+++ src/loading/loading.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './loading.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LoadingComponent
+  ],
+  declarations: [
+    LoadingComponent
+  ],
+  providers: [],
+})
+export class LoadingModule { }
