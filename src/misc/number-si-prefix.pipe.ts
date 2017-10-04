912fee14f73d0a23120c7beaeaf5cbebef248000
Alvaro Saburido <alvaro.saburido@gmail.com>
2017-10-04 02:41:07 +0200

    feature/CAN-57 SI prefix on Count To

--- src/misc/number-si-prefix.pipe.ts
+++ src/misc/number-si-prefix.pipe.ts
@@ -4,9 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'numberSiPrefix'
 })
 export class NumberSiPrefixPipe implements PipeTransform {
-  public transform(value: number, digits: string): any {
+  public transform(value: any, digits: string): any {
     // tslint:disable-next-line:curly
-    if (!value) return value;
+    if (!value || value === undefined) return value;
+    if (typeof(value) === 'string') {
+        value = parseInt(value.replace(',', ''), 0);
+    }
     const si = [
         { value: 1E18, symbol: 'E' },
         { value: 1E15, symbol: 'P' },
